82f842757f2b83044c1d938437656be7cf1dd69e
liuyanguang <liuyanguang@qiyi.com>
2018-04-11 10:08:42 +0800

    add all project

---
+++ src/main/java/com/iqiyi/ConfigClientApplication.java
@@ -0,0 +1,12 @@
+package com.iqiyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+}
